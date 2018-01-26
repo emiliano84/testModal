bc09b9f6811a7e5077af78ae4efd8ccddfb7a4d5
Emiliano Magliocca <Emiliano.Magliocca@deltatre.com>
2018-01-26 16:32:13 +0000

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Modal()
+        {
+            return PartialView("_DeleteCategory");
+        }
+    }
+}
---
+++ WebApplication1/Models/BootstrapModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication1.Models
+{
+    public class BootstrapModel
+    {
+        public string Id { get; set; }
+        public string AreaLabeledId { get; set; }
+        public string Message { get; set; }
+    }
+}
